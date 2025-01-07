f99b3b6f5f9615d6fd34ad65b82528714b44dcac
chichardanthony <chichardanthony@gmail.com>
2025-01-07 12:02:07 -0700

    Update Button.js

--- components/Button.js
+++ components/Button.js
@@ -1,1 +1,12 @@
+import React from 'react';
 
+const Button = ({ children, onClick }) => (
+  <button 
+    onClick={onClick} 
+    className="bg-primary text-white font-bold py-2 px-4 rounded hover:bg-accent transition duration-300"
+  >
+    {children}
+  </button>
+);
+
+export default Button;
