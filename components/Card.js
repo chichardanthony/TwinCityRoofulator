8c095c3a3dc893c4d87c028b09dfd145e566cb1c
chichardanthony <chichardanthony@gmail.com>
2025-01-07 12:07:47 -0700

    Update Card.js

--- components/Card.js
+++ components/Card.js
@@ -1,1 +1,9 @@
+import React from 'react';
 
+const Card = ({ children }) => (
+  <div className="bg-white rounded-lg shadow p-6 hover:shadow-lg transition-shadow duration-300">
+    {children}
+  </div>
+);
+
+export default Card;
